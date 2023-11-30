205411d7b339c1e57ac88bd65251810e36bec110
TrentonMobly <89452025+TrentonMobly@users.noreply.github.com>
2023-11-30 09:52:39 +0100

    Add files via upload

---
+++ btc.py
@@ -0,0 +1,26 @@
+import csv
+import re
+
+def trova_indirizzi_btc(file_csv, colonna_testo):
+    indirizzi_btc = set()
+
+    with open(file_csv, 'r', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        
+        for riga in reader:
+            testo = riga[colonna_testo]
+            indirizzi_trovati = re.findall(r'\b[13][a-km-zA-HJ-NP-Z1-9]{25,34}\b', testo)
+
+            indirizzi_btc.update(indirizzi_trovati)
+
+    return indirizzi_btc
+
+# Esempio di utilizzo:
+file_csv_input = 'file.csv'
+colonna_testo_input = 'USER ATTRIBUTES ***;;;;;;;'
+
+indirizzi_trovati = trova_indirizzi_btc(file_csv_input, colonna_testo_input)
+
+print("Indirizzi BTC trovati:")
+for indirizzo in indirizzi_trovati:
+    print(indirizzo)
